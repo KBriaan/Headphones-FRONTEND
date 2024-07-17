625172d0e1a304e219bad12e93148a66017fce0b
K Briaan <kbriaan1@gmail.com>
2024-07-17 12:17:16 +0300

    Update navbar with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { UpdateFollower } from "react-mouse-follower";
 import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
   return (
    <main className="overflow-x-hidden">
-<Navbar/>
+    <UpdateFollower mouseOptions={{
+      backgroundColor:'white',
+      zIndex:999,
+      followSpeed:1.5,
+    }} >
+    <Navbar/>
+
+    </UpdateFollower>
    </main>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { IoMdMenu } from "react-icons/io";
 import { SlEarphones } from "react-icons/sl";
+import { UpdateFollower } from "react-mouse-follower";
+import { motion } from "framer-motion";
 const NavbarMenu = [
   {
     id: 1,
@@ -31,38 +33,78 @@ const NavbarMenu = [
 const Navbar = () => {
   return (
     <>
-      <div className="bg-brandDark text-white py-4">
-        <nav className="flex justify-between items-center">
+      <div className="bg-brandDark text-white py-8 font-varela">
+        <motion.nav initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{duration:1, delay:0.5}}
+        className="container flex justify-between items-center">
           {/* logo section */}
-          <div>
-            <a href="#" className="text-xl font-bold uppercase">
-              Playing/
-              <span className="font-extralight text-white/70">Market</span>
-            </a>
-          </div>
+
+          <UpdateFollower
+                mouseOptions={{
+                  backgroundColor: "white",
+                  zIndex: 999,
+                  followSpeed: 1.5,
+                  scale: 5,
+                  mixBlendMode: "difference",
+                }}>  <div>
+                <a href="#" className="text-xl font-bold uppercase">
+                  Playing/
+                  <span className="font-extralight text-white/70">Market</span>
+                </a>
+              </div> </UpdateFollower>
+         
           {/* menu section  */}
           <div className="hidden md:block">
             <ul className="flex items-center gap-4">
               {NavbarMenu.map((item) => (
                 <li key={item.id}>
-                  <a
+                  <UpdateFollower
+                    mouseOptions={{
+                      backgroundColor: "white",
+                      zIndex: 999,
+                      followSpeed: 1.5,
+                      scale: 5,
+                      mixBlendMode: "difference",
+                    }}
+                  >
+                    <a
+                      href={item.link}
+                      className="inline-block text-sm py-2 px-3 uppercase"
+                    >
+                      {item.title}
+                    </a>
+                  </UpdateFollower>
+                  {/* <a
                     href={item.link}
                     className="inline-block text-sm py-2 px-3 uppercase"
                   >
                     {item.title}
-                  </a>
+                  </a> */}
                 </li>
               ))}
-              <SlEarphones />
+              <UpdateFollower
+                mouseOptions={{
+                  backgroundColor: "white",
+                  zIndex: 999,
+                  followSpeed: 1.5,
+                  scale: 5,
+                  mixBlendMode: "difference",
+                }}
+              >
+                <button className="text-xl ps-14">
+                  <SlEarphones />
+                </button>
+              </UpdateFollower>
             </ul>
 
             <div></div>
           </div>
           {/* mobile hambuger section */}
-          <div>
-            <IoMdMenu />
+          <div className="md:hidden">
+            <IoMdMenu className="text-4xl" />
           </div>
-        </nav>
+        </motion.nav>
       </div>
     </>
   );
